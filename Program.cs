d57897e1c63628b7a8ea4780e78432e5a49dcf6e
Thamires Giovani <thamires-giovani@hotmail.com>
2021-06-24 12:05:59 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Desafios.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
